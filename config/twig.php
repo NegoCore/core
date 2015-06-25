73c9b5c4717096e25f8ffcba08cb464e1c4212c5
Iván Molina Pavana <montemolina@live.com>
2015-06-25 17:45:47 -0500

    Se agrega la configuración para el módulo Twig

---
+++ config/twig.php
@@ -0,0 +1,41 @@
+<?php defined('NEGOCORE') or die('No direct script access.');
+
+/**
+ * Skeleton Module
+ *
+ * @author      Iván Molina Pavana <imp@negocad.mx>
+ * @copyright   Copyright (c) 2015, NegoCAD <info@negocad.mx>
+ * @version     1.0.0
+ */
+
+// --------------------------------------------------------------------------------
+
+/**
+ * Twig Module Configs
+ *
+ * @filesource
+ */
+return array(
+
+    /**
+     * Twig Environment options
+     *
+     * http://twig.sensiolabs.org/doc/api.html#environment-options
+     */
+    'environment' => array(
+        'autoescape'          => false,
+    ),
+
+    /**
+     * Custom functions, filters and tests
+     *
+     *     'functions' => array(
+     *         'my_method' => array('MyClass', 'my_method'),
+     *     ),
+     */
+    'functions' => array(),
+    'filters' => array(
+        'without' => array('Twig_Filters', 'without')
+    ),
+    'tests' => array(),
+);
