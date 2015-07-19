d1a5bd2bcc3dee1adeb74a1e8204c84c93b9f507
JNeutron <montemolina@live.com>
2015-07-18 20:30:08 -0500

    Se agregan funciones al ORM

---
+++ classes/NegoCore/ORM.php
@@ -0,0 +1,16 @@
+<?php defined('NEGOCORE') or die('No direct script access.');
+
+/**
+ * NegoCore
+ *
+ * @author      Iván Molina Pavana <imp@negocad.mx>
+ * @copyright   Copyright (c) 2015, NegoCAD <info@negocad.mx>
+ * @version     1.0.0
+ */
+
+// --------------------------------------------------------------------------------
+
+/**
+ * Class URL
+ */
+class NegoCore_ORM extends Kohana_ORM {}
